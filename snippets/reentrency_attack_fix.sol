7f2223d70d010b020ca9c910d803d5ed55dbaa50
Your Name <you@example.com>
2025-04-03 19:51:17 +0300

    add commit

--- snippets/reentrency_attack_fix.sol
+++ snippets/reentrency_attack_fix.sol
@@ -8,12 +8,12 @@ contract PiggyBank is ReentrancyGuard {
         credit[msg.sender] += msg.value;
     }
 
-function withdraw(uint amount) public nonReentrant {
-    require(credit[msg.sender] >= amount, "Insufficient balance");
-    
-    credit[msg.sender] -= amount; // Effect: Update state before transferring funds
-    
-    (bool success,) = payable(msg.sender).call{value: amount}(""); // Interaction: Transfer funds last
-    require(success, "Transfer failed");
-}
+    function withdraw(uint amount) public nonReentrant {
+        require(credit[msg.sender] >= amount, "Insufficient balance");
+        
+        credit[msg.sender] -= amount; // Effect: Update state before transferring funds
+        
+        (bool success,) = payable(msg.sender).call{value: amount}(""); // Interaction: Transfer funds last
+        require(success, "Transfer failed");
+    }
 }
